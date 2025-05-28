f5bf1e650d1b8054987bb0f6ce268ecae232aad3
Lalu-Oji-Darmaji <laluojidarmaji@gmail.com>
2025-05-28 14:20:24 +0800

    lalu oji darmaji

--- lalu_oji_darmaji/routes/web.php
+++ lalu_oji_darmaji/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,11 +7,6 @@ Route::get('/', function () {
 });
 
 Route::get('/request', function (Request $request) {
-    // $nama_gambar = $_FILES["foto"]["name"];
-    // $lokasi_gambar = $_FILES["foto"]["tmp_name"];
-    // move_uploaded_file($lokasi_gambar, "storage/" . $nama_gambar);
-    // $foto = $nama_gambar;
-    // $foto = $_GET['foto'];
     $nama = $_GET['nama'];
     $umur = $_GET['umur'];
     $prodi = $_GET['prodi'];
